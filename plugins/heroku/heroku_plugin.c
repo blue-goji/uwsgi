be91de0fbb9b4cef0a4a466c73591b86a03212b1
Steven Collins <stevencollins@bluegoji.com>
2014-01-10 17:29:06 -0600

    Very minor update (maybe just block SIGTERM instead of special handling?...)

--- plugins/heroku/heroku_plugin.c
+++ plugins/heroku/heroku_plugin.c
@@ -4,10 +4,10 @@ extern struct uwsgi_server uwsgi;
 
 // A bit of a hack; it's not really a "hijacking" as it's used in the other
 // plugins. But the hijack_worker plugin hook is the only one executed
-// after initial UNIX signal handling is set for generic plugins.
+// after initial UNIX signal handling is set for generic plugins, and it 
+// seems safest to set this as late as possible.
 void uwsgi_heroku_hijack(void) {
-	// at the very least, reset SIGTERM handler to an alarm of some
-	// sort (just use harakiri?)
+	uwsgi_block_signal(SIGTERM)
 	return;
 }
 
